d629ae34a12a8067dc3a55737ff30cab74bafa86
Ruben Tytgat <ruben.tytgat@gmail.com>
2013-10-10 10:26:59 +0200

    Routing helpers and better server/gs
    Added helpers for easy routing of parameters in Ruby and Python
    scripts.
    
    Improved server. Modified gs to be more REST and to use the routing
    helper.

--- server.py
+++ server.py
@@ -12,13 +12,14 @@ class Script(Resource):
 	def find_suitable_script(self, patharray):
 		for i in reversed(range(len(patharray) + 1)):
 			potential_path_array = ['scripts'] + patharray[:i]
+			remainder_array = patharray[i:]
 			potential_path = apply(path.join, potential_path_array)
 
 			if not path.exists(potential_path):
 				continue
 
 			if path.isfile(potential_path):
-				return potential_path
+				return (potential_path,remainder_array)
 
 			if path.isdir(potential_path):
 				main_path = apply(path.join, potential_path_array + ['main'])
@@ -27,66 +28,73 @@ class Script(Resource):
 					continue
 
 				if path.isfile(main_path):
-					return main_path
+					return (main_path,remainder_array)
 
 		raise BaseException("Unknown script")
 
 	def render(self, request):
-		the_script = self.find_suitable_script(request.postpath)
+		wrapper_script = path.join(".","script_wrapper")
+		(the_script,the_args) = self.find_suitable_script(request.postpath)
 		content_type = request.getHeader("Content-Type") or "text/plain"
 		
 		p = ScriptProtocol(request)
 		reactor.spawnProcess(p, 
-		                    the_script,
-		                    [the_script] + [request.method,content_type])
+		                    wrapper_script,
+		                    [wrapper_script,the_script,request.method,content_type] + the_args,
+		                    None)
 		return server.NOT_DONE_YET
 
 class ScriptProtocol(protocol.ProcessProtocol, pb.Viewable):
-    errortext = ''
+	got_content_type = False
+	content_type = ''
+	errortext = ''
 
-    def view_resumeProducing(self, issuer):
-        self.resumeProducing()
+	def view_resumeProducing(self, issuer):
+		self.resumeProducing()
 
-    def view_pauseProducing(self, issuer):
-        self.pauseProducing()
+	def view_pauseProducing(self, issuer):
+		self.pauseProducing()
 
-    def view_stopProducing(self, issuer):
-        self.stopProducing()
+	def view_stopProducing(self, issuer):
+		self.stopProducing()
 
-    def resumeProducing(self):
-        self.transport.resumeProducing()
+	def resumeProducing(self):
+		self.transport.resumeProducing()
 
-    def pauseProducing(self):
-        self.transport.pauseProducing()
+	def pauseProducing(self):
+		self.transport.pauseProducing()
 
-    def stopProducing(self):
-        self.transport.loseConnection()
+	def stopProducing(self):
+		self.transport.loseConnection()
 
-    def __init__(self, request):
-        self.request = request
+	def __init__(self, request):
+		self.request = request
 
-    def connectionMade(self):
-        self.request.registerProducer(self, 1)
-        self.request.content.seek(0, 0)
-        content = self.request.content.read()
-        if content:
-            self.transport.write(content)
-        self.transport.closeStdin()
+	def connectionMade(self):
+		self.request.registerProducer(self, 1)
+		self.request.content.seek(0, 0)
+		content = self.request.content.read()
+		if content:
+			self.transport.write(content)
+		self.transport.closeStdin()
 
-    def errReceived(self, error):
-        self.errortext = self.errortext + error
+	def errReceived(self, error):
+		self.errortext = self.errortext + error
 
-    def outReceived(self, output):
-        self.request.write(output)
+	def outReceived(self, output):
+#		if not got_content_type:
 
-    def processEnded(self, reason):
-        if reason.value.exitCode != 0:
-            log.msg("Script %s exited with exit code %s" %
-                    (self.request.uri, reason.value.exitCode))
-        if self.errortext:
-            log.msg("Errors from script %s: %s" % (self.request.uri, self.errortext))
-        self.request.unregisterProducer()
-        self.request.finish()
+		self.request.write(output)
+
+	def processEnded(self, reason):
+		if reason.value.exitCode != 0:
+			log.msg("Script %s exited with exit code %s" %
+					(self.request.uri, reason.value.exitCode))
+		if self.errortext:
+			log.err("Errors from script %s: %s" % (self.request.uri, self.errortext))
+		self.request.unregisterProducer()
+		self.request.finish()
+
 
 root = Script()
 factory = server.Site(root)
---
+++ util/routing/routing.rb
@@ -0,0 +1,50 @@
+module Routing
+	class Router
+		def match(rule,options = {})
+			realargs = ARGV[2..-1]
+
+			incoming_method = ARGV[0].downcase.to_sym
+			return if options[:via] != :all and options[:via] != incoming_method and not options[:via].include? incoming_method
+			return if realargs.size() != rule.size()
+
+			bindings = match_rule(rule,realargs,options)
+			if bindings
+				@routed = true
+				yield bindings
+			end
+		end
+
+		def method_missing(methId,rule,options = {})
+			options[:via] = [methId]
+			match(rule,options) { |b| yield b }
+		end
+
+		def routed?
+			@routed
+		end
+
+		private
+
+		def match_rule(rule,realargs,options = {})
+			bindings = {}
+			rule.zip(realargs).each do |rule_el,realarg|
+				case rule_el
+				when Symbol
+					return false if not options[rule_el].nil? and not realarg =~ options[rule_el]
+					bindings[rule_el] = realarg
+				when String
+					return false if rule_el != realarg
+				else
+					raise "Illegal rule for router match: #{rule}"
+				end
+			end
+			return bindings
+		end
+	end
+
+	def self.route
+		router = Router.new
+		yield router
+		raise "Couldn't find a route" if not router.routed?
+	end
+end
