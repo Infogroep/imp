99480ff8a28b698019fe5fd19fad57b976805dd9
Ruben Tytgat <ruben.tytgat@gmail.com>
2013-10-16 06:00:25 +0200

    Youtube Search

--- util/youtube/youtube_api.rb
+++ util/youtube/youtube_api.rb
@@ -3,6 +3,9 @@ require 'google/api_client'
 
 class YoutubeAPI < Google::APIClient
 	def initialize
-		super key: "AIzaSyDkktCySGJkQFlWxqpOwUOEE_6gHlPg4qA", authorization: nil
+		super application_name: "imp",
+		      application_version: "0.1",
+		      key: "AIzaSyDkktCySGJkQFlWxqpOwUOEE_6gHlPg4qA",
+		      authorization: nil
 	end
 end
